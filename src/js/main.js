501537076cea5e767fc89baaaf2a7f21d1f79f58
revolman <1revolman1@gmail.com>
2020-11-03 00:11:49 +0200

    make css prototype for oppening nav

--- src/js/main.js
+++ src/js/main.js
@@ -13,5 +13,15 @@ $(document).ready(() => {
       console.log(`ELSE PAGE`);
       break;
   }
-  [1, 2, 3].map((e) => e ** 2);
+
+  function headerJS() {
+    document.querySelectorAll("ul li.dropup .dropbtn").forEach((button) => {
+      button.addEventListener("click", function (event) {
+        console.log("CLICKED", button?.parentElement?.childNodes[0])
+        button?.parentElement?.childNodes[1].firstChild.classList.toggle("unshow")
+        button?.parentElement?.childNodes[0].classList.toggle("unshow")
+      })
+    })
+  }
+  headerJS();
 });
