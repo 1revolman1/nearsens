8dd936053c563ac43416bd794360c9b21f921625
revolman <1revolman1@gmail.com>
2020-11-06 22:34:31 +0200

    make product list without responsive

--- src/js/product_page/index.js
+++ src/js/product_page/index.js
@@ -39,4 +39,31 @@ export const useProductPage=function main() {
             });
         }
     });
+    const scrollable=document.querySelector("section.productpage__pageheader_sticky")
+    function onScrollChange(changes, observer) {
+        changes.forEach(change => {
+        if (change.intersectionRatio===0) {
+            scrollable.classList.add("out-viewport")
+            scrollable.style.opacity="1";
+            scrollable.style.visibility="visible";
+
+            console.log('Header is outside viewport');
+            }else{
+            scrollable.classList.remove("out-viewport")
+            scrollable.style.opacity="0";
+            scrollable.style.visibility="hidden";
+            console.log('Header is IN THE viewport');
+            }
+            
+        });
+    }
+    const options = {
+        root: null, //root
+        rootMargin: '-100px',
+        threshold: 0,
+    };
+    
+    const observer = new IntersectionObserver(onScrollChange, options);
+    const target = document.querySelector('section.productpage__pageheader');
+    if(target) observer.observe(target);
 }
