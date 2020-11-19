9eb8c92c1ba79ba6d275699a13717738c8a747c2
revolman <1revolman1@gmail.com>
2020-11-19 13:08:19 +0200

    index page without parrallax

--- src/js/home_page/index.js
+++ src/js/home_page/index.js
@@ -1,55 +1,91 @@
 export const useHomePage = function () {
     
-    function onYouTubeIframeAPIReady() {
-        window.YT.ready(function() {
-            window.player = new YT.Player("player-container", {
-                videoId, // YouTube Video ID
-                width: 640, // Player width (in px)
-                 events: {
-                    'onReady':onPlayerReady,
-                 },
-                playerVars: {
-                    autoplay: 0, // Auto-play the video on load
-                    loop: 1, // Run the video in a loop
-                    mute: 1, // Sound Off On
-                    rel: 0,
-                    controls: 0, // Show pause/play buttons in player
-                    showinfo: 0, // Hide the video title
-                    modestbranding: 1, // Hide the Youtube Logo
-                    fs: 0, // Hide the full screen button
-                    cc_load_policy: 0, // Hide closed captions
-                    iv_load_policy: 0, // Hide the Video Annotations
-                    autohide: 1, // Hide video controls when playing
-                },
-            });
-        });
-      }
-    function onPlayerReady(event) {
-        console.log("EVENT",event)
-        // event.target.setVolume(100);
-        event.target.playVideo();
-        function onScrollChange(changes, observer) {
-            changes.forEach(change => {
-                if (change.intersectionRatio===0) {
-                    event.target.pauseVideo();
-                }
-                else{
-                    event.target.playVideo();
-                }
-            });
-        }
-        const options = {
-            root: null, //root
-            rootMargin: '0%',
-            threshold: 0,
-        };
-        const observer = new IntersectionObserver(onScrollChange, options);
+    // function onYouTubeIframeAPIReady() {
+    //     window.YT.ready(function() {
+    //         window.player = new YT.Player("player-container", {
+    //             videoId, // YouTube Video ID
+    //             width: 640, // Player width (in px)
+    //              events: {
+    //                 'onReady':onPlayerReady,
+    //              },
+    //             playerVars: {
+    //                 autoplay: 0, // Auto-play the video on load
+    //                 loop: 1, // Run the video in a loop
+    //                 mute: 1, // Sound Off On
+    //                 rel: 0,
+    //                 controls: 0, // Show pause/play buttons in player
+    //                 showinfo: 0, // Hide the video title
+    //                 modestbranding: 1, // Hide the Youtube Logo
+    //                 fs: 0, // Hide the full screen button
+    //                 cc_load_policy: 0, // Hide closed captions
+    //                 iv_load_policy: 0, // Hide the Video Annotations
+    //                 autohide: 1, // Hide video controls when playing
+    //             },
+    //         });
+    //     });
+    // }
+    // function onPlayerReady(event) {
+    //     console.log("EVENT",event)
+    //     // event.target.setVolume(100);
+    //     event.target.playVideo();
+    //     function onScrollChange(changes, observer) {
+    //         changes.forEach(change => {
+    //             if (change.intersectionRatio===0) {
+    //                 event.target.pauseVideo();
+    //             }
+    //             else{
+    //                 event.target.playVideo();
+    //             }
+    //         });
+    //     }
+    //     const options = {
+    //         root: null, //root
+    //         rootMargin: '0%',
+    //         threshold: 0,
+    //     };
+    //     const observer = new IntersectionObserver(onScrollChange, options);
       
-        const target = document.querySelector('.index__youtubeframe');
-        if(target) observer.observe(target);
-    }
-    onYouTubeIframeAPIReady();
+    //     const target = document.querySelector('.index__youtubeframe');
+    //     if(target) observer.observe(target);
+    // }
+    // onYouTubeIframeAPIReady();
     
+    const video = document.querySelector("div#player-container video");
+    video.pause();
+    function onScrollChange(changes, observer) {
+        changes.forEach(change => {
+            if (change.intersectionRatio===0) {
+                video.pause();
+            }
+            else{
+                video.play();
+            }
+        });
+    }
+    const options = {
+        root: null, //root
+        rootMargin: '0%',
+        threshold: 0,
+    };
+    const observer = new IntersectionObserver(onScrollChange, options);
+    
+    const target = document.querySelector('.index__youtubeframe');
+    if(target) observer.observe(target);
+
+    // const video = document.querySelector(".video-container video");
+    // video.play();
+    // const observer = new IntersectionObserver((entries) => {
+    // entries.forEach((entry) => {
+    //     if (!entry.isIntersecting) {
+    //     video.pause();
+    //     } else {
+    //     video.play();
+    //     }
+    // });
+    // }, {});
+
+    // observer.observe(video);
+
     // $(window).on('scroll load', function(){
     //     $('.index__youtubeframe__frame iframe').each( function(i){
     //       player.pauseVideo();
