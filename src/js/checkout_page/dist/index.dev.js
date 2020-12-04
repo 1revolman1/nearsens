8425db11678afa2dd043a31964763358c34d0e57
revolman <1revolman1@gmail.com>
2020-12-04 23:37:31 +0200

    make some new additional changes

--- src/js/checkout_page/dist/index.dev.js
+++ src/js/checkout_page/dist/index.dev.js
@@ -6,12 +6,33 @@ Object.defineProperty(exports, "__esModule", {
 exports.useCheckoutPage = void 0;
 
 var useCheckoutPage = function useCheckoutPage() {
-  console.log('CHECKOUT');
-  document.querySelectorAll('.checkout_page__content__contact__manip__chekedbox__box').forEach(function (elm) {
+  function openOne(cls) {
+    var remove = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
+    document.querySelectorAll(cls).forEach(function (elm) {
+      elm.addEventListener('change', function (e) {
+        var _this = this;
+
+        document.querySelectorAll(cls).forEach(function (other) {
+          if (_this !== other && other.checked) {
+            other.checked = false;
+            if (remove) other.closest('label').classList.remove('checked');
+          }
+        });
+      });
+    });
+  }
+
+  document.querySelectorAll('label').forEach(function (elm) {
     elm.addEventListener('change', function (e) {
       this.classList.toggle('checked');
     });
-  });
+  }); //Contact page
+
+  openOne('.checkout_page__checkbox input', true); //Main service
+
+  openOne('.checkout_page__content__contact__manip__chekedbox__box input', true); //Bill page
+
+  openOne('.checkout_page__content__contact__manip__payMethod__elm input', true);
 };
 
 exports.useCheckoutPage = useCheckoutPage;
--- src/js/checkout_page/index.js
+++ src/js/checkout_page/index.js
@@ -1,10 +1,32 @@
 export const useCheckoutPage = () => {
-  console.log('CHECKOUT');
-  document
-    .querySelectorAll('.checkout_page__content__contact__manip__chekedbox__box')
-    .forEach((elm) => {
+  function openOne(cls, remove = false) {
+    document.querySelectorAll(cls).forEach(function (elm) {
       elm.addEventListener('change', function (e) {
-        this.classList.toggle('checked');
+        document.querySelectorAll(cls).forEach((other) => {
+          if (this !== other && other.checked) {
+            other.checked = false;
+            if (remove) other.closest('label').classList.remove('checked');
+          }
+        });
       });
     });
+  }
+  document.querySelectorAll('label').forEach((elm) => {
+    elm.addEventListener('change', function (e) {
+      this.classList.toggle('checked');
+    });
+  });
+
+  //Contact page
+  openOne('.checkout_page__checkbox input', true);
+  //Main service
+  openOne(
+    '.checkout_page__content__contact__manip__chekedbox__box input',
+    true
+  );
+  //Bill page
+  openOne(
+    '.checkout_page__content__contact__manip__payMethod__elm input',
+    true
+  );
 };
