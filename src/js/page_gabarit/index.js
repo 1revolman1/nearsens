2913ba2bac1c3ec08e0c1369fe6e45b3a5d70120
revolman <1revolman1@gmail.com>
2021-02-04 13:04:20 +0200

    make hero block at gabarit page

--- src/js/page_gabarit/index.js
+++ src/js/page_gabarit/index.js
@@ -1,5 +1,8 @@
 export const usePageGabarit = function main() {
   console.log('PAGE GABARIT');
+  //add class for page gabarit
+  if (document.querySelectorAll('.custom-block').length > 0)
+    document.querySelector('main').classList.add('custom-page');
   document
     .querySelectorAll('section.page_gabarit__fourblocks')
     .forEach(function (container) {
