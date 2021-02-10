f1937b135be2ecc7cece65ef43fc1f27225e24a7
revolman <1revolman1@gmail.com>
2021-02-10 10:01:57 +0200

    add image grid block

--- src/js/solution_page/dist/index.dev.js
+++ src/js/solution_page/dist/index.dev.js
@@ -7,6 +7,12 @@ exports.useSolutionPage = void 0;
 
 var useSolutionPage = function useSolutionPage() {
   console.log('Solution Page');
+  document.querySelectorAll('section.solutionpage__imggrid .solutionpage__imggrid__grid__img').forEach(function (block) {
+    console.log('TEST');
+    block.addEventListener('click', function () {
+      block.classList.toggle('active');
+    });
+  });
 };
 
 exports.useSolutionPage = useSolutionPage;
--- src/js/solution_page/index.js
+++ src/js/solution_page/index.js
@@ -1,3 +1,13 @@
 export const useSolutionPage = function () {
   console.log('Solution Page');
+  document
+    .querySelectorAll(
+      'section.solutionpage__imggrid .solutionpage__imggrid__grid__img'
+    )
+    .forEach((block) => {
+      console.log('TEST');
+      block.addEventListener('click', function () {
+        block.classList.toggle('active');
+      });
+    });
 };
